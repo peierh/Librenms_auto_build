6cc010c22d9c8ff87e6922f4dcbff58cd85f821c
andy212130 <quan_xi87@yahoo.com.tw>
2019-12-09 15:58:06 +0800

    addhost v2

--- auto_build/add_host.sh
+++ auto_build/add_host.sh
@@ -2,4 +2,5 @@
 ip=$(hostname -I)
 echo $ip
 
-sudo sed -i "34c \$config['nets'][] = \"$ip/32\"; "
+#sudo sed -i "34c \$config['nets'][] = \"$ip/32\"; " /opt/librenms/config.php
+/opt/librenms/snmp-scan.py
