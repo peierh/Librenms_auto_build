a34810f521f0247d562fbed1998ad20bd7ce20cc
andy212130 <quan_xi87@yahoo.com.tw>
2019-12-16 13:35:22 +0800

    v1.3 addhost community fix

--- auto_build/auto_build.sh
+++ auto_build/auto_build.sh
@@ -210,7 +210,7 @@ ip=$(hostname -I)
 lenth=${#ip}
 ipnew=${ip:0:lenth-1}
 
-sudo sed -i "33c \$config['snmp']['community'][] = \"a123456\";" /opt/librenms/config.php
+sudo sed -i "33c \$config['snmp']['community'][] = \"${comm}\";" /opt/librenms/config.php
 sudo sed -i "34c \$config['nets'][] = \"$ipnew/32\"; " /opt/librenms/config.php
 sudo sed -i "35c \$config['autodiscovery']['nets-exclude'][] = '$ipnew/32';" /opt/librenms/config.php
 sudo sed -i "36c \$config['allow_duplicate_sysName'] = true; " /opt/librenms/config.php
