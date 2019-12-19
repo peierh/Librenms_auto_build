ac4acdf6031e17a2729bec8a31703d643cb559de
andy212130 <quan_xi87@yahoo.com.tw>
2019-12-19 10:46:51 +0800

    v1.4 addhost new

--- auto_build/auto_build.sh
+++ auto_build/auto_build.sh
@@ -212,7 +212,7 @@ ipnew=${ip:0:lenth-1}
 
 sudo sed -i "33c \$config['snmp']['community'][] = \"${comm}\";" /opt/librenms/config.php
 sudo sed -i "34c \$config['nets'][] = \"$ipnew/32\"; " /opt/librenms/config.php
-sudo sed -i "35c \$config['autodiscovery']['nets-exclude'][] = '$ipnew/32';" /opt/librenms/config.php
+#sudo sed -i "35c \$config['autodiscovery']['nets-exclude'][] = '$ipnew/32';" /opt/librenms/config.php
 sudo sed -i "36c \$config['allow_duplicate_sysName'] = true; " /opt/librenms/config.php
 sudo sed -i "37c \$config['discovery_by_ip'] = true; " /opt/librenms/config.php
 sudo sed -i "38c \$config['discovery_modules']['discover-arp'] = true; " /opt/librenms/config.php
