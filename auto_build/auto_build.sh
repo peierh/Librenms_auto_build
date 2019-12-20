0fe137073859e7fbbae71712fe6bcb25e4da11db
andy212130 <quan_xi87@yahoo.com.tw>
2019-12-20 15:01:37 +0800

    v2.2 auto grafana new

--- auto_build/auto_build.sh
+++ auto_build/auto_build.sh
@@ -219,9 +219,8 @@ sudo sed -i "38c \$config['discovery_modules']['discover-arp'] = true; " /opt/li
 /opt/librenms/snmp-scan.py
 
 echo ==================== Grafana Built =======================
-sudo git clone https://github.com/j13tw/School_Monitor_System.git /home/pi/School_Monitor_System/
-sudo python3 /home/pi/School_Monitor_System/Client/enviroment.py
-sudo nohup python3 -u /home/pi/School_Monitor_System/Client/selfCheck.py ${sn} > /home/pi/client.log 2>&1 &
+sudo python3 /home/pi/Librenms_auto_build/Client/enviroment.py
+sudo nohup python3 -u /home/pi/Librenms_auto_build/Client/selfCheck.py ${sn} > /home/pi/client.log 2>&1 &
 
 
 echo ==================== Install Complete ====================
--- auto_build/auto_grafana.sh
+++ auto_build/auto_grafana.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo git clone https://github.com/j13tw/School_Monitor_System.git /home/pi/
-sudo python3 /home/pi/School_Monitor_System/Client/enviroment.py
-sudo nohup python3 -u /home/pi/School_Monitor_System/Client/selfCheck.py 7777 > /home/pi/client.log 2>&1 &
+#sudo git clone https://github.com/j13tw/School_Monitor_System.git /home/pi/
+read -p "請輸入學校代碼：" sn;
+sudo python3 /home/pi/Librenms_auto_build/Client/enviroment.py
+sudo nohup python3 -u /home/pi/Librenms_auto_build/Client/selfCheck.py ${sn} > /home/pi/client.log 2>&1 &
 
