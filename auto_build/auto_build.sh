41acb791bc81fe94169e8634df0ef7da4d095bdb
andy212130 <quan_xi87@yahoo.com.tw>
2019-12-20 11:55:12 +0800

    v2.2 git path change

--- auto_build/auto_build.sh
+++ auto_build/auto_build.sh
@@ -219,7 +219,7 @@ sudo sed -i "38c \$config['discovery_modules']['discover-arp'] = true; " /opt/li
 /opt/librenms/snmp-scan.py
 
 echo ==================== Grafana Built =======================
-sudo git clone https://github.com/j13tw/School_Monitor_System.git /home/pi/
+sudo git clone https://github.com/j13tw/School_Monitor_System.git /home/pi/School_Monitor_System/
 sudo python3 /home/pi/School_Monitor_System/Client/enviroment.py
 sudo nohup python3 -u /home/pi/School_Monitor_System/Client/selfCheck.py ${sn} > /home/pi/client.log 2>&1 &
 
