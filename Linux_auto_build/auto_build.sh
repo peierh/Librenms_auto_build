54d2fbe7fa9167712dee57ed2bfad9f8dc66f5ab
andy212130 <quan_xi87@yahoo.com.tw>
2020-03-20 08:10:15 +0000

    path x86_PC

--- Linux_auto_build/auto_build.sh
+++ Linux_auto_build/auto_build.sh
@@ -222,8 +222,8 @@ sudo sed -i "38c \$config['discovery_modules']['discover-arp'] = true; " /opt/li
 
 echo ==================== Grafana Built =======================
 sudo git clone https://github.com/j13tw/School_Monitor_System.git /home/ubuntu/School_Monitor_System
-sudo sed -i "3c command=python3 selfCheck.py $comm" /home/ubuntu/School_Monitor_System/Client/client.conf 
-sudo python3 /home/ubuntu/School_Monitor_System/Client/enviroment.py
+sudo sed -i "3c command=python3 selfCheck.py $comm" /home/ubuntu/School_Monitor_System/Client/x86_PC/client.conf 
+sudo python3 /home/ubuntu/School_Monitor_System/Client/x86_PC/enviroment.py
 #sudo python3 /home/pi/Librenms_auto_build/Client/enviroment.py
 #sudo nohup python3 -u /home/pi/Librenms_auto_build/Client/selfCheck.py ${sn} > /home/pi/client.log 2>&1 &
 
