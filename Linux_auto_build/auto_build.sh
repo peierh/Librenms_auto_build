797ab7c0fa4d3dcd2b430924a5afc05544b665bd
andy212130 <quan_xi87@yahoo.com.tw>
2020-05-20 13:19:39 +0800

    fix Linux

--- Linux_auto_build/auto_build.sh
+++ Linux_auto_build/auto_build.sh
@@ -208,12 +208,12 @@ sudo /etc/init.d/cron restart
 sudo /opt/librenms/adduser.php ${uiuser} ${uipwd} 10 ${uiemail}
 
 #add host 
-ip=$(hostname -I)
-lenth=${#ip}
-ipnew=${ip:0:lenth-1}
+#ip=$(hostname -I)
+#lenth=${#ip}
+#ipnew=${ip:0:lenth-1}
 
 sudo sed -i "33c \$config['snmp']['community'][] = \"${comm}\";" /opt/librenms/config.php
-sudo sed -i "34c \$config['nets'][] = \"$ipnew/32\"; " /opt/librenms/config.php
+#sudo sed -i "34c \$config['nets'][] = \"$ipnew/32\"; " /opt/librenms/config.php
 #sudo sed -i "35c \$config['autodiscovery']['nets-exclude'][] = '$ipnew/32';" /opt/librenms/config.php
 sudo sed -i "36c \$config['allow_duplicate_sysName'] = true; " /opt/librenms/config.php
 sudo sed -i "37c \$config['discovery_by_ip'] = true; " /opt/librenms/config.php
