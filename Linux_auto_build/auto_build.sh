8a309cd1835abd3014a733db8c12bbe5571e2a26
VP <51seer.vincent@gmail.com>
2020-12-30 03:36:44 +0900

    hided the unnecessary output for end-user

--- Linux_auto_build/auto_build.sh
+++ Linux_auto_build/auto_build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo ==================== Start Install ====================
+# setup install log placement
+logPath="/tmp/client.log"
+echo "--> Installation export to >> $logPath"
+echo "--> Get install log by using \"tail -f " $logPath "\""
+echo "------------------------------------------------------"
+
+echo ==================== 安裝開始 ====================
 
 # change timezon
-sudo cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime
+sudo cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime >> $logPath
 #
 while true;
 do
@@ -81,9 +87,9 @@ uipwd=${sn}pass
 #read -p "輸入LibreNMS使用者密碼: " uipwd;
 
 sudo chmod 777 Librenms_auto_build/auto_build/config.json
-sudo sed -i "3c \"name\":\"${dbuser}\"," Librenms_auto_build/auto_build/config.json
-sudo sed -i "7c \"user\":\"${uiuser}\"," Librenms_auto_build/auto_build/config.json
-sudo sed -i "9c \"database\":\"${dbname}\"," Librenms_auto_build/auto_build/config.json
+sudo sed -i "3c \"name\":\"${dbuser}\"," Librenms_auto_build/auto_build/config.json >> $logPath
+sudo sed -i "7c \"user\":\"${uiuser}\"," Librenms_auto_build/auto_build/config.json >> $logPath 
+sudo sed -i "9c \"database\":\"${dbname}\"," Librenms_auto_build/auto_build/config.json >> $logPath
 
 #change time
 #while true;
@@ -99,76 +105,98 @@ sudo sed -i "9c \"database\":\"${dbname}\"," Librenms_auto_build/auto_build/conf
 #	fi
 #done
 
-#date -s "$time"
-sudo apt update
+sudo apt update >> $logPath
 
 #start ssh
-/etc/init.d/ssh start
+echo start ssh >> $logPath
+/etc/init.d/ssh start >> $logPath
 
 # install packages
 echo
-echo ==================== Step1: Download and Install Packages ===================
-#sudo apt install software-properties-common -y
-#sudo apt-get install apt-transport-https lsb-release ca-certificates -y
-#sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-#echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-sudo apt-get update
-sudo add-apt-repository ppa:ondrej/php
-#sudo apt-get install php7.2-cli -y
-sudo apt update
-sudo apt install php7.2 -y
-sudo apt install influxdb influxdb-client -y
-sudo apt install vim curl apache2 composer fping git graphviz imagemagick libapache2-mod-php7.2 mariadb-client mariadb-server mtr-tiny nmap php7.2-cli php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mysql php7.2-snmp php7.2-xml php7.2-zip python-memcache python-mysqldb rrdtool snmp snmpd whois -y
-sudo apt install python3 python3-pip python3-dev -y
-# add librenms user
-sudo useradd librenms -d /opt/librenms -M -r
-sudo usermod -a -G librenms www-data
+echo ==================== Step1: 下載並安裝必要套件 ===================
+echo 作業系統更新
+echo "$(date '+%Y-%m-%d %H:%M:%S')    apt update" >> $logPath
+sudo apt-get update >> $logPath
 
-echo ============================================ download librenms ===========================================
+echo 安裝必要套件
+echo "$(date '+%Y-%m-%d %H:%M:%S')    add-apt-repository ppa:ondrej/php" >> $logPath
+sudo add-apt-repository ppa:ondrej/php -y >> $logPath
+sudo apt update
+
+echo 安裝php
+echo "$(date '+%Y-%m-%d %H:%M:%S')    apt install php7.2" >> $logPath
+sudo apt install php7.2 -y >>  $logPath
+
+echo 安裝influxdb
+echo "$(date '+%Y-%m-%d %H:%M:%S')    apt install influxdb" >> $logPath
+sudo apt install influxdb influxdb-client -y >> $logPath
+
+echo 安裝python相關套件
+echo "$(date '+%Y-%m-%d %H:%M:%S')    apt install python3" >> $logPath
+sudo apt install python3 python3-pip python3-dev -y >> $logPath
+
+echo 安裝其他必要套件
+echo "$(date '+%Y-%m-%d %H:%M:%S')    apt install other packages" >> $logPath
+sudo apt install vim curl apache2 composer acl fping git graphviz imagemagick libapache2-mod-php7.2 mariadb-client mariadb-server mtr-tiny nmap php7.2-cli php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mysql php7.2-snmp php7.2-xml php7.2-zip python-memcache python-mysqldb rrdtool snmp snmpd whois -y >> $logPath
+
+# add librenms user
+echo 新增 librenms 使用者
+echo "$(date '+%Y-%m-%d %H:%M:%S')    add librenms user" >> $logPath
+sudo useradd librenms -d /opt/librenms -M -r >> $logPath
+sudo usermod -a -G librenms www-data >> $logPath
+
+echo ============================================ 下載 librenms ===========================================
 sudo chmod 777 /opt
 cd /opt
-sudo git clone https://github.com/andy212130/librenms.git
+echo 下載 librenms
+echo "$(date '+%Y-%m-%d %H:%M:%S')    git clone NMS" >> $logPath
+sudo git clone https://github.com/andy212130/librenms.git >> $logPath
 echo ===========================================================
 cd /opt/librenms
-sudo git checkout 1_62
+sudo git checkout 1_62 >> $logPath
 cd /opt
 # set permissions
-sudo apt install acl -y
-sudo chown -R librenms:librenms /opt/librenms
-sudo chmod 777 /opt/librenms
-sudo setfacl -d -m g::rwx /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/
-sudo setfacl -R -m g::rwx /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/
+echo 挑整 librenms 權限
+echo "$(date '+%Y-%m-%d %H:%M:%S')    set permissions" >> $logPath
+sudo chown -R librenms:librenms /opt/librenms >> $logPath
+sudo chmod 777 /opt/librenms >> $logPath
+sudo setfacl -d -m g::rwx /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/ >> $logPath
+sudo setfacl -R -m g::rwx /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/ >> $logPath
 
 # install librenms
 echo
-echo ==================== Step2: Install LibreNMS  ====================
+echo ==================== Step2: 安裝 LibreNMS  ====================
 cd /opt/librenms
-sudo ./scripts/composer_wrapper.php self-update --1
-sudo ./scripts/composer_wrapper.php install --no-dev
+echo "$(date '+%Y-%m-%d %H:%M:%S')    LibreNMS installation" >> $logPath
+sudo ./scripts/composer_wrapper.php self-update --1 >> $logPath
+sudo ./scripts/composer_wrapper.php install --no-dev >> $logPath
 # configure mysql
 echo
-echo ==================== Step3: Set Database Config  ====================
-sudo systemctl restart mysql
-
-sudo mysql --user="$root" --password=" "  --execute="CREATE DATABASE ${dbname} CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
-sudo mysql --user="$root" --password=" "  --execute="CREATE USER '${dbuser}'@'%' IDENTIFIED BY '${dbpass}';"
-sudo mysql --user="$root" --password=" "  --execute="GRANT ALL PRIVILEGES ON ${dbname}.* TO '${dbuser}'@'%';"
-sudo mysql --user="$root" --password=" "  --execute="FLUSH PRIVILEGES;"
-
-sudo sed -i "13c innodb_file_per_table=1" /etc/mysql/mariadb.conf.d/50-server.cnf
-sudo sed -i "13a lower_case_table_names=0" /etc/mysql/mariadb.conf.d/50-server.cnf
-sudo sed -i "31c #bind-address       = 127.0.0.1" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-sudo systemctl restart mysql
+echo ==================== Step3: 設定資料庫  ====================
+sudo systemctl restart mysql >> $logPath
+echo 設定資料庫權限
+echo "$(date '+%Y-%m-%d %H:%M:%S')    DB setup" >> $logPath
+sudo mysql --user="$root" --password=" "  --execute="CREATE DATABASE ${dbname} CHARACTER SET utf8 COLLATE utf8_unicode_ci;" >> $logPath
+sudo mysql --user="$root" --password=" "  --execute="CREATE USER '${dbuser}'@'%' IDENTIFIED BY '${dbpass}';" >> $logPath
+sudo mysql --user="$root" --password=" "  --execute="GRANT ALL PRIVILEGES ON ${dbname}.* TO '${dbuser}'@'%';" >> $logPath
+sudo mysql --user="$root" --password=" "  --execute="FLUSH PRIVILEGES;" >> $logPath
+sudo sed -i "13c innodb_file_per_table=1" /etc/mysql/mariadb.conf.d/50-server.cnf >> $logPath
+sudo sed -i "13a lower_case_table_names=0" /etc/mysql/mariadb.conf.d/50-server.cnf >> $logPath
+sudo sed -i "31c #bind-address       = 127.0.0.1" /etc/mysql/mariadb.conf.d/50-server.cnf >> $logPath
+sudo systemctl restart mysql >> $logPath
 
 # configure php
-sudo sed -i "941c date.timezone = "Asia/Taipei"" /etc/php/7.2/apache2/php.ini
-sudo sed -i "941c date.timezone = "Asia/Taipei"" /etc/php/7.2/cli/php.ini
+echo 設定php
+echo "$(date '+%Y-%m-%d %H:%M:%S')    configure php" >> $logPath
+sudo sed -i "941c date.timezone = "Asia/Taipei"" /etc/php/7.2/apache2/php.ini >> $logPath
+sudo sed -i "941c date.timezone = "Asia/Taipei"" /etc/php/7.2/cli/php.ini >> $logPath
 
-sudo a2enmod php7.2
-sudo a2dismod mpm_event
-sudo a2enmod mpm_prefork
+sudo a2enmod php7.2 >> $logPath
+sudo a2dismod mpm_event >> $logPath
+sudo a2enmod mpm_prefork >> $logPath
 
+echo 設定apache
+echo "$(date '+%Y-%m-%d %H:%M:%S')    configure apache" >> $logPath
 sudo cat > /etc/apache2/sites-available/librenms.conf <<EOF
 <VirtualHost *:80>
   DocumentRoot /opt/librenms/html/
@@ -183,119 +211,92 @@ sudo cat > /etc/apache2/sites-available/librenms.conf <<EOF
 </VirtualHost>
 EOF
 
-sudo a2dissite 000-default
-sudo a2ensite librenms.conf
-sudo a2enmod rewrite
-sudo systemctl restart apache2
+sudo a2dissite 000-default >> $logPath
+sudo a2ensite librenms.conf >> $logPath
+sudo a2enmod rewrite >> $logPath
+sudo systemctl restart apache2 >> $logPath
 
 # configure snmp
-sudo cp /opt/librenms/snmpd.conf.example /etc/snmp/snmpd.conf
-
-sudo sed -i "2c com2sec readonly  default        ${comm} # RANDOMSTRINGGOESHERE" /etc/snmp/snmpd.conf
-
-sudo curl -o /usr/bin/distro https://raw.githubusercontent.com/librenms/librenms-agent/master/snmp/distro
-sudo chmod +x /usr/bin/distro
-sudo systemctl restart snmpd
+echo 設定snmp
+echo "$(date '+%Y-%m-%d %H:%M:%S')    configure snmp" >> $logPath
+sudo cp /opt/librenms/snmpd.conf.example /etc/snmp/snmpd.conf >> $logPath
+sudo sed -i "2c com2sec readonly  default        ${comm} # RANDOMSTRINGGOESHERE" /etc/snmp/snmpd.conf >> $logPath
+sudo curl -o /usr/bin/distro https://raw.githubusercontent.com/librenms/librenms-agent/master/snmp/distro >> $logPath
+sudo chmod +x /usr/bin/distro >> $logPath
+sudo systemctl restart snmpd >> $logPath
 
 # cron job
-sudo cp /opt/librenms/librenms.nonroot.cron /etc/cron.d/librenms
+echo 設定系統例行性工作
+echo "$(date '+%Y-%m-%d %H:%M:%S')    cron job" >> $logPath
+sudo cp /opt/librenms/librenms.nonroot.cron /etc/cron.d/librenms >> $logPath
+sudo bash -c '(echo "0 0 5 * * root [ -f \"/home/$(logname)/client.log.1\" ] && rm /home/$(logname)/client.log.*" >> /etc/crontab)'
 
 # copy logrotate congfig
-sudo cp /opt/librenms/misc/librenms.logrotate /etc/logrotate.d/librenms
+echo "$(date '+%Y-%m-%d %H:%M:%S')    copy logrotate congfig" >> $logPath
+sudo cp /opt/librenms/misc/librenms.logrotate /etc/logrotate.d/librenms >> $logPath
 
 # setting webui config.php
-sudo cp /opt/librenms/config.php.default /opt/librenms/config.php
+echo "$(date '+%Y-%m-%d %H:%M:%S')    setting webui config.php" >> $logPath
+sudo cp /opt/librenms/config.php.default /opt/librenms/config.php >> $logPath
 
-sudo sed -i "6c \$config['db_host'] = 'localhost';" /opt/librenms/config.php
-sudo sed -i "7c \$config['db_user'] = '${dbuser}';" /opt/librenms/config.php
-sudo sed -i "8c \$config['db_pass'] = '${dbpass}';" /opt/librenms/config.php
-sudo sed -i "9c \$config['db_name'] = '${dbname}';" /opt/librenms/config.php
+sudo sed -i "6c \$config['db_host'] = 'localhost';" /opt/librenms/config.php >> $logPath
+sudo sed -i "7c \$config['db_user'] = '${dbuser}';" /opt/librenms/config.php >> $logPath
+sudo sed -i "8c \$config['db_pass'] = '${dbpass}';" /opt/librenms/config.php >> $logPath
+sudo sed -i "9c \$config['db_name'] = '${dbname}';" /opt/librenms/config.php >> $logPath
 
 #change mode from librenms
-sudo chmod 777 /opt
-sudo chmod 777 /opt/librenms
-sudo chmod 777 /opt/librenms/logs/librenms.log
-sudo chown -R librenms:librenms /opt/librenms
-sudo setfacl -d -m g::rwx /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/
-sudo chmod -R ug=rwX /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/
+echo "$(date '+%Y-%m-%d %H:%M:%S')    change mode from librenms" >> $logPath
+sudo chmod 777 /opt >> $logPath
+sudo chmod 777 /opt/librenms >> $logPath
+sudo chmod 777 /opt/librenms/logs/librenms.log >> $logPath
+sudo chown -R librenms:librenms /opt/librenms >> $logPath
+sudo setfacl -d -m g::rwx /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/ >> $logPath
+sudo chmod -R ug=rwX /opt/librenms/rrd /opt/librenms/logs /opt/librenms/bootstrap/cache/ /opt/librenms/storage/ >> $logPath
 
 # Transfer to influxdb
-sudo echo "\$config['influxdb']['enable'] = true;" >> /opt/librenms/config.php
-sudo echo "\$config['influxdb']['transport'] = 'http';" >> /opt/librenms/config.php  # Default, other options: https, udp
-sudo echo "\$config['influxdb']['host'] = '127.0.0.1';" >> /opt/librenms/config.php
-sudo echo "\$config['influxdb']['port'] = '8086';" >> /opt/librenms/config.php # http:8086 https:8088
-sudo echo "\$config['influxdb']['db'] = '${dbname}';" >> /opt/librenms/config.php
-sudo echo "\$config['influxdb']['username'] = '${dbuser}';" >> /opt/librenms/config.php
-sudo echo "\$config['influxdb']['password'] = '${dbpass}';" >> /opt/librenms/config.php
-sudo echo "\$config['influxdb']['timeout'] = 0;" >> /opt/librenms/config.php # Optional
-sudo echo "\$config['influxdb']['verifySSL'] = false;" >> /opt/librenms/config.php # Optional
-
-# final step
-#sudo chown librenms:librenms /opt/librenms/config.php
-
-
+echo 設定 influxdb
+echo "$(date '+%Y-%m-%d %H:%M:%S')    Transfer to influxdb" >> $logPath
+sudo echo "\$config['influxdb']['enable'] = true;" >> /opt/librenms/config.php >> $logPath
+sudo echo "\$config['influxdb']['transport'] = 'http';" >> /opt/librenms/config.php  >> $logPath# Default, other options: https, udp
+sudo echo "\$config['influxdb']['host'] = '127.0.0.1';" >> /opt/librenms/config.php >> $logPath
+sudo echo "\$config['influxdb']['port'] = '8086';" >> /opt/librenms/config.php >> $logPath # http:8086 https:8088
+sudo echo "\$config['influxdb']['db'] = '${dbname}';" >> /opt/librenms/config.php >> $logPath
+sudo echo "\$config['influxdb']['username'] = '${dbuser}';" >> /opt/librenms/config.php >> $logPath
+sudo echo "\$config['influxdb']['password'] = '${dbpass}';" >> /opt/librenms/config.php >> $logPath
+sudo echo "\$config['influxdb']['timeout'] = 0;" >> /opt/librenms/config.php >> $logPath # Optional
+sudo echo "\$config['influxdb']['verifySSL'] = false;" >> /opt/librenms/config.php >> $logPath # Optional
 
 # sql backup
 sudo cat > /opt/sql_bk.sh <<EOF
 #!/bin/bash
 
 sudo mysqldump -u root -p123456 ${dbname} > sqlbk_`date +"%Y-%m-%d"`.sql
-#sudo mysql -h -u librenms -p 123456 ${dbname} < sqlbk_`date +"%Y-%m-%d"`.sql
 EOF
 
 sudo /opt/librenms/lnms migrate
 
 sudo echo "*/10  *    * * *   root    /opt/sql_bk.sh" >> /etc/crontab
 
-sudo /etc/init.d/cron restart
-
-#修改config.json檔
-#cd 
-#cd Librenms_auto_build/auto_build/
-
-#sudo chmod 777 ./config.json
-#sudo sed -i "3c \"name\":\"${dbuser}\"," ./config.json
-#sudo sed -i "7c \"user\":\"${uiuser}\"," ./config.json
-#sudo sed -i "9c \"database\":\"${dbname}\"," ./config.json
-#sudo chmod 777 Librenms_auto_build/auto_build/config.json
-#sudo sed -i "3c \"name\":\"${dbuser}\"," Librenms_auto_build/auto_build/config.json
-#sudo sed -i "7c \"user\":\"${uiuser}\"," Librenms_auto_build/auto_build/config.json
-#sudo sed -i "9c \"database\":\"${dbname}\"," Librenms_auto_build/auto_build/config.json
+sudo /etc/init.d/cron restart >> $logPath
 
 #add user
-sudo /opt/librenms/adduser.php ${uiuser} ${uipwd} 10 ${uiemail}
+echo 新增 librenms 使用者
+sudo /opt/librenms/adduser.php ${uiuser} ${uipwd} 10 ${uiemail}  >> $logPath
 
-#add host 
-#ip=$(hostname -I)
-#lenth=${#ip}
-#ipnew=${ip:0:lenth-1}
-
-sudo sed -i "33c \$config['snmp']['community'][] = \"${comm}\";" /opt/librenms/config.php
-#sudo sed -i "34c \$config['nets'][] = \"$ipnew/32\"; " /opt/librenms/config.php
-#sudo sed -i "35c \$config['autodiscovery']['nets-exclude'][] = '$ipnew/32';" /opt/librenms/config.php
-sudo sed -i "36c \$config['allow_duplicate_sysName'] = true; " /opt/librenms/config.php
-sudo sed -i "37c \$config['discovery_by_ip'] = true; " /opt/librenms/config.php
-sudo sed -i "38c \$config['discovery_modules']['discover-arp'] = true; " /opt/librenms/config.php
+sudo sed -i "33c \$config['snmp']['community'][] = \"${comm}\";" /opt/librenms/config.php >> $logPath
+sudo sed -i "36c \$config['allow_duplicate_sysName'] = true; " /opt/librenms/config.php >> $logPath
+sudo sed -i "37c \$config['discovery_by_ip'] = true; " /opt/librenms/config.php >> $logPath
+sudo sed -i "38c \$config['discovery_modules']['discover-arp'] = true; " /opt/librenms/config.php >> $logPath
 #/opt/librenms/snmp-scan.py
 
-echo ==================== Grafana Built =======================
-sudo git clone https://github.com/j13tw/School_Monitor_System.git /home/ubuntu/School_Monitor_System
-sudo sed -i "3c command=python3 selfCheck.py $comm $cs $sip" /home/ubuntu/School_Monitor_System/Client/x86_PC/client.conf 
-sudo python3 /home/ubuntu/School_Monitor_System/Client/x86_PC/environment.py
-#sudo python3 /home/pi/Librenms_auto_build/Client/environment.py
-#sudo nohup python3 -u /home/pi/Librenms_auto_build/Client/selfCheck.py ${sn} > /home/pi/client.log 2>&1 &
+echo ==================== 安裝Grafana =======================
+echo 下載Grafana
+sudo git clone https://github.com/j13tw/School_Monitor_System.git /home/ubuntu/School_Monitor_System  >> $logPath
+sudo sed -i "3c command=python3 selfCheck.py $comm $cs $sip" /home/ubuntu/School_Monitor_System/Client/x86_PC/client.conf   >> $logPath
+echo 執行安裝
+sudo python3 /home/ubuntu/School_Monitor_System/Client/x86_PC/environment.py  >> $logPath
 
 
-echo ==================== Install Complete ====================
+echo ==================== 安裝完成 ====================
 echo
 echo "請於瀏覽器輸入IP"
-# rrd back up
-#sudo chmod 777 /opt/librenms/extra_code/rrdtool_dump.sh
-#sudo chmod 777 /opt/librenms/extra_code/sql_upload.sh
-#sudo mkdir /opt/librenms/extra_code/xml && sudo chmod 777 /opt/librenms/extra_code/xml
-
-#sudo echo "0  *    * * *   root    /opt/librenms/extra_code/rrdtool_dump.sh" >> /etc/crontab
-#sudo echo "*/10  *    * * *   root    /opt/librenms/extra_code/sql_upload.sh" >> /etc/crontab
-
-#autoclean client.log
-sudo bash -c '(echo "0 0 5 * * root [ -f \"/home/$(logname)/client.log.1\" ] && rm /home/$(logname)/client.log.*" >> /etc/crontab)'
